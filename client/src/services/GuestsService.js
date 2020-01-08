e79426498a4a97a8606d2f08376648439b9aa3ae
Brendan <laotsu@hotmail.co.uk>
2020-01-08 14:23:27 +0000

    Methods for index, delete and create requests added

--- client/src/services/GuestsService.js
+++ client/src/services/GuestsService.js
@@ -1,5 +1,27 @@
 const baseURL = 'http://localhost:3000/api/guests/'
 
-export default{
-  
+export default {
+  getGuests(){
+    return fetch(baseURL)
+    .then(res => res.json())
+  },
+  postGuest(payload){
+    return fetch(baseURL, {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+  },
+  // updateGuest(id){
+  //   return fetch(baseURL + id, {
+  //     method: 'PUT',
+  //     body:
+  //   })
+  // },
+  deleteGuest(id){
+    return fetch(baseURL + id, {
+      method: 'DELETE'
+    });
+  }
 }
